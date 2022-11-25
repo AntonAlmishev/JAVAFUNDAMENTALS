c35311df8fdb51717a0cfab47147f5758be5eba4
Anton Almishev <119037539+AntonAlmishev@users.noreply.github.com>
2022-11-25 11:19:31 +0200

    Add files via upload

---
+++ DrawSquare.java
@@ -0,0 +1,15 @@
+package CodeLessons15Cycles;
+
+public class DrawSquare {
+    public static void main(String[] args) {
+
+        int element = 3;
+
+        for (int i = 1; i <=3; i++) {
+            for ( int j = 1; j <= 3; j++){
+                System.out.print("#"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
